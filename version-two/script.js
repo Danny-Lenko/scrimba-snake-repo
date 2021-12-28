d40045f6910060c2eb91d0e00cbe0206a7283e19
DannyLenk <danylenko.14@gmail.com>
2021-12-28 10:28:05 +0200

    updated script.js
    managed to render snake

--- version-two/script.js
+++ version-two/script.js
@@ -2,17 +2,28 @@
 
 window.onload = function() {
    view.renderGrid();
+   view.renderSnake();
 }
 
 let view = {
+   snake: [2, 1, 0],
 
    renderGrid: function() {
       const gridEl = document.querySelector('.gridEl');
-      const square = `<div class='square'><div>`;
       for (let i = 0; i < 100; i++) {
-         gridEl.innerHTML += square;
+         const square = document.createElement('div');
+         gridEl.appendChild(square);
+         square.setAttribute('class', 'square');
+      }
+   },
+
+   renderSnake: function() {
+      const gridElList = document.getElementsByClassName('square');
+      for (let i = 0; i < this.snake.length; i++) {
+         gridElList[this.snake[i]].classList.add('snake');
       }
    }
+
 };
 
 
